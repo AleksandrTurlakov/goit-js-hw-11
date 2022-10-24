1581b86f2361734f17fa2798dccdd2534610ce0f
AleksandrTurlakov <105392594+AleksandrTurlakov@users.noreply.github.com>
2022-10-24 15:37:24 +0000

    Deploying to gh-pages from @ AleksandrTurlakov/goit-js-hw-11@d425c1b5650cd440814b0717aae683ba63d04701 🚀

--- index.5a25b28d.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".search-form");console.log(o),console.log("Hello world");
-//# sourceMappingURL=index.5a25b28d.js.map
--- index.e6ca46e5.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".search-form");console.log(o),console.log("Hello world")}();
-//# sourceMappingURL=index.e6ca46e5.js.map
